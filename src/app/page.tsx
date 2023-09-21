50668e23ac4e492a1925005d3b1349243f6a95e7
Ovikx <ovikdas10@gmail.com>
2023-09-20 22:43:59 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/login">Log In</Link>
+    </main>
+  );
+}
